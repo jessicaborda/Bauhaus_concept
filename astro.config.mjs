437ef2bf4ad981dc4aad773c3d4707fc094a483c
--get <--get>
2025-12-02 19:12:26 -0500

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    api: 'modern-compiler', // Recommended for modern Sass
-                },
-            },
-        },
-    },
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  api: 'modern-compiler', // Recommended for modern Sass
+              },
+          },
+      },
+  },
+
+  integrations: [react()],
 });
